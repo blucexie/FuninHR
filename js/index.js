593c7a16292090551abb1118dd7aab37071cc7c8
谢玉琪 <649453836@qq.com>
2017-07-18 11:25:14 +0800

    2017.07.18

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+
+
+$.noConflict();
+jQuery('.zy-Slide').zySlide({ speed: 500 })
+.css('border', '0px solid red');
+    // $('#Slide2').zySlide({delay : 2000,speed : 2000})
+
+    // var divs = $('.zy-Slide')
+    // divs.each(function(i, ele){
+    //     Slide(ele)
+    // })
+    // (function () {
+    //     alert(arguments[0])
+    // })
+
+    // (function () {
+    //     alert(1)
+    // })
+jQuery(window).scroll(function (){
+    var clientH = jQuery(window).height();
+    var scrollH = jQuery(window).scrollTop();
+    var checkoutT = jQuery('.checkout').offset().top;
+    var orderInterviewT = jQuery('.orderInterview').offset().top;
+    if(clientH+scrollH>=checkoutT){
+        jQuery('#content .empower .sidebar').slideDown(1000)
+    }
+    if(clientH+scrollH>=orderInterviewT){
+
+        jQuery('#content .checkout .sidebar').animate({
+            opacity:1
+        },1500)
+    }
+});
